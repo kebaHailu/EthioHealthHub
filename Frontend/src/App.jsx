9493c918996b692a344ce4701d74da4cbbc37d80
gebby123 <tefayegebrehiwot123@gmail.com>
2024-04-16 21:35:13 +0300

    Vite configured

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+  <h1> HELLO  THIS IS TEH INTRODUCTION OF OUR HEALTH APP</h1>   
+   </div>
+  
+    </>
+  )
+}
+
+export default App
