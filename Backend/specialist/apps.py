0e2f46fb5c23183d979947f9d73173858fab6479
kebaHailu <kibromhailu.5513@gmail.com>
2024-04-29 13:13:34 +0300

    add specialist and modify station model for privilage

---
+++ Backend/specialist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SpecialistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'specialist'
--- Backend/station/models.py
+++ Backend/station/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 class Station(models.Model):
     name = models.CharField(max_length=255)
     admin_name = models.CharField(max_length=255)
-    cover_image = models.CharField(max_length=255)
+    cover_image = models.CharField(max_length=255, null=True, blank=True)
     location = models.CharField(max_length=255)
-    latitude = models.CharField(max_length=300)
-    longitude = models.CharField(max_length=300)
+    latitude = models.CharField(max_length=300, null=True, blank=True)
+    longitude = models.CharField(max_length=300, null=True, blank=True)
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    approved_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    approved_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True, related_name='approved_stations')
     description = models.TextField()
     password = models.CharField(max_length=500)
 
