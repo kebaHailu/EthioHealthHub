10edc9e778594f92c6bea1987f8e002fe55b4593
kebaHailu <kibromhailu.5513@gmail.com>
2024-05-01 11:43:12 +0300

    Create core admin and models for user

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -1,7 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.db import models
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
 
--- Backend/station/models.py
+++ Backend/station/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
 
 
@@ -13,7 +13,7 @@ class Station(models.Model):
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    approved_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True, related_name='approved_stations')
+    approved_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=True, null=True, related_name='approved_stations')
     description = models.TextField()
     password = models.CharField(max_length=500)
 
