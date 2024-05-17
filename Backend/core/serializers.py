3858f885dd7653aa49ec1983b350fa6a658d584b
kebaHailu <kibromhailu.5513@gmail.com>
2024-05-18 00:33:45 +0300

    Create a view for appointment including specialist id and station id

--- Backend/core/serializers.py
+++ Backend/core/serializers.py
@@ -1,13 +1,25 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Appointment, Prescription
 
 
-class AppointmentSerializer(ModelSerializer):
+class AppointmentSerializer(serializers.ModelSerializer):
+    patient_name = serializers.SerializerMethodField()
+    gender = serializers.CharField(source='clinical_record.patient.gender')
+    station_name = serializers.CharField(source='technician.station.name')
+    technician_first_name = serializers.CharField(source='technician.user.first_name')
+    technician_last_name = serializers.CharField(source='technician.user.last_name')
+    specialist_first_name = serializers.CharField(source='specialist.user.first_name')
+    specialist_last_name = serializers.CharField(source='specialist.user.last_name')
+    def get_patient_name(self, obj):
+        return f"{obj.clinical_record.patient.first_name} {obj.clinical_record.patient.last_name}"
+
     class Meta:
         model = Appointment
-        fields = '__all__'
+        fields = ['patient_name', 'appointment_date', 'status', 'gender', 'station_name', 'technician_first_name', 'technician_last_name',
+                  'specialist_first_name', 'specialist_last_name']
 
-class PrescriptionSerializer(ModelSerializer):
+
+class PrescriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prescription
         fields = '__all__'
--- Backend/core/views.py
+++ Backend/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Appointment, Prescription
 from .serializers import AppointmentSerializer, PrescriptionSerializer
 
@@ -12,4 +12,19 @@ class AppointmentViewSet(viewsets.ModelViewSet):
 class PrescriptionViewSet(viewsets.ModelViewSet):
     queryset = Prescription.objects.all()
     serializer_class = PrescriptionSerializer
+
+
+class SpecialistAppointmentAPIView(generics.ListAPIView):
+    serializer_class = AppointmentSerializer
+
+    def get_queryset(self):
+        specialist_id = self.kwargs['specialist_id']
+        return Appointment.objects.filter(specialist_id=specialist_id)
+
+
+class StationAppointmentAPIView(generics.ListAPIView):
+    serializer_class = AppointmentSerializer
+    def get_queryset(self):
+        station_id = self.kwargs['station_id']
+        return Appointment.objects.filter(technician__station_id=station_id)
 
