f99cf2bd9e19b2663bb159d5bf9d3fa1ff06d22d
kebaHailu <kibromhailu.5513@gmail.com>
2024-05-18 14:54:38 +0300

    create the functional appointment

--- Backend/core/serializers.py
+++ Backend/core/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
+
+from specialist.serializers import SpecialistSerializer
 from .models import Appointment, Prescription
+from technician.serializers import PatientSerializer, ClinicalRecordSerializer
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = '__all__'
+
+
+class AppointmentGetSerializer(serializers.ModelSerializer):
     patient_name = serializers.SerializerMethodField()
     gender = serializers.CharField(source='clinical_record.patient.gender')
     station_name = serializers.CharField(source='technician.station.name')
@@ -11,6 +20,7 @@ class AppointmentSerializer(serializers.ModelSerializer):
     specialist_first_name = serializers.CharField(source='specialist.user.first_name')
     specialist_last_name = serializers.CharField(source='specialist.user.last_name')
     specialization = serializers.CharField(source='specialist.specialization')
+
     def get_patient_name(self, obj):
         return f"{obj.clinical_record.patient.first_name} {obj.clinical_record.patient.last_name}"
 
@@ -20,8 +30,18 @@ class AppointmentSerializer(serializers.ModelSerializer):
                   'specialist_first_name', 'specialist_last_name']
 
 
+class AppointmentDetailSerializer(serializers.ModelSerializer):
+    #patient = PatientSerializer()
+    clinical_record = ClinicalRecordSerializer()
+    specialist = SpecialistSerializer()
+
+    class Meta:
+        model = Appointment
+        fields = ['clinical_record', 'clinical_record', 'specialist', 'appointment_date', 'message', 'status']
+
 
 class PrescriptionSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Prescription
         fields = '__all__'
